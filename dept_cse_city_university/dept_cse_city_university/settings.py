093b394cd5cc0db2183b6370fae606ba4e0ab516
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2024-11-11 08:37:47 -0600

    add host

--- dept_cse_city_university/dept_cse_city_university/settings.py
+++ dept_cse_city_university/dept_cse_city_university/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = 'django-insecure-dxmj(=31%*fua@0-v^0@_(0x2$)tp#lzpvxumf8oc!mj*uit(5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app']
+
 
 AUTH_USER_MODEL = "user.User"
 # Application definition
