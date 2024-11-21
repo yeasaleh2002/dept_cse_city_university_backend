768b1ec1baf56e880df47f2eb9a4690db666a121
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2024-11-21 05:37:00 -0600

    postgraSQL database add

--- dept_cse_city_university/dept_cse_city_university/settings.py
+++ dept_cse_city_university/dept_cse_city_university/settings.py
@@ -96,23 +96,23 @@ WSGI_APPLICATION = 'dept_cse_city_university.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres.igofirpuwdjebitupukw',
-#         'PASSWORD': 'fazlerabbi1972',
-#         'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
-#         'PORT': '6543'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.igofirpuwdjebitupukw',
+        'PASSWORD': 'fazlerabbi1972',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543'
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
